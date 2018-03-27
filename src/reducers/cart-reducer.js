acc6e4ddbc62030e9335d563df6a1b5673400422
Dan Coman <dancoman@Dans-MacBook-Air.local>
2018-03-27 12:56:11 +0800

    cleanup

--- src/reducers/cart-reducer.js
+++ src/reducers/cart-reducer.js
@@ -13,20 +13,6 @@ const initialState = {
   ]
 }
 
-// export const cartReducer = (state=initialState, action) => {
-//   switch (action.type) {
-//     case 'ADD_TO_CART': {
-//       return {
-//         ...state,
-//         cart: [...state.cart, action.payload]
-//       }
-//     }
-
-//     default:
-//       return state;
-//   }
-// }
-
 export default (state=initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_CART': {
